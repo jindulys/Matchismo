1108cd9003ca231edc71c7b6fd6175f13ba55ae0
lys <857367901@qq.com>
2013-02-05 16:20:58 +0800

    add a button

--- Matchismo/LysViewController.m
+++ Matchismo/LysViewController.m
@@ -18,8 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSString *str = @"this is a test";
-    NSLog(@"show the text %@",str);
+    
 }
 
 - (void)didReceiveMemoryWarning
